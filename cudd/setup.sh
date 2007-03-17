7a5097e768c24c7965d5bbbe83cf0c04d1b5eeda
Dave Parker <dave.x.parker@gmail.com>
2007-03-17 00:21:36 +0000

    Added dddmp package back into CUDD.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@241 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- cudd/setup.sh
+++ cudd/setup.sh
@@ -9,7 +9,7 @@ if test -d include
     $CREATE ../cudd/cudd.h .
     $CREATE ../cudd/cuddInt.h .
     $CREATE ../epd/epd.h .
-    #$CREATE ../dddmp/dddmp.h .
+    $CREATE ../dddmp/dddmp.h .
     $CREATE ../mtr/mtr.h .
     #$CREATE ../obj/cuddObj.hh .
     $CREATE ../st/st.h .
@@ -25,6 +25,7 @@ if test -d lib
     cd lib
     $CREATE ../cudd/libcudd.a .
     $CREATE ../epd/libepd.a .
+    $CREATE ../dddmp/libdddmp.a .
     $CREATE ../mtr/libmtr.a .
     $CREATE ../st/libst.a .
     $CREATE ../util/libutil.a .
