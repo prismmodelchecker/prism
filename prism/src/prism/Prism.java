e80af1a74bf038f5801f5359dee2c44301351836
Dave Parker <dave.x.parker@gmail.com>
2007-04-10 09:04:34 +0000

    Make sure 3.1->3.1.1 changes included in trunk.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@256 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/prism/Prism.java
+++ prism/src/prism/Prism.java
@@ -46,7 +46,7 @@ import simulator.*;
 public class Prism implements PrismSettingsListener
 {
 	// prism version
-	private static String version = "3.1.dev";
+	private static String version = "3.1.1.dev";
 	
 	//------------------------------------------------------------------------------
 	// Constants
