5545eba87d69beaf0c523f73195d0c00a9c3a72f
Gethin Norman <gethin.norman@glasgow.ac.uk>
2008-12-05 16:42:18 +0000

    updated rabin example
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@872 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism-examples/rabin/.rabinN.nm.pp
+++ prism-examples/rabin/.rabinN.nm.pp
@@ -2,7 +2,18 @@
 #const K=4+func(ceil,func(log,N,2))#
 // N-processor mutual exclusion [Rab82]
 // with global variables to remove sychronization 
-// gxn/dxp 02/02/00
+// gxn/dxp 03/12/08
+
+// version to verify a variant of the bounded waiting property
+// namely the probability a process entries the critical section given it tries
+// to acheive this we have split the drawing phase into two steps, so we know
+// a process will draw before it actual makes the probabilistic choice
+// we have however kept the two steps atomic(no other process can move one
+// the first step has been made) since otherwise the adversary can prevent the
+// process from actually trying in the current round by never shcedling it again
+
+// note we have removed the self loops to eliminate the need for fairness constraints 
+
 
 mdp
 
@@ -27,37 +38,43 @@ global r : [1..2];
 // current draw: bi
 // current round: ri
 
-// atomic formula for process 1 drawing
+// formula for process 1 drawing
 formula draw = p1=1 & (b<b1 | r!=r1);
+
+// formula to keep drawing phase atomic
+// (can onlymove if no process is drawing
+formula go = (#& i=2:N#draw#i#=0#end#);
 
 module process1
 
 	p1 : [0..2];
 	b1 : [0..K];
 	r1 : [0..2];
+	draw1 : [0..1];
 
 	// remain in remainder
-	[] p1=0 -> (p1'=0);
+	// [] go & p1=0 -> (p1'=0);
 	// enter trying
-	[] p1=0 -> (p1'=1);
+	[] go & p1=0 -> (p1'=1);
 	// make a draw
-	[] draw ->#+ i=1:K-1# #1/func(floor,func(pow,2,i))# : (b1'=#i#) & (r1'=r) & (b'=max(b,#i#))
-	         #end#+ #1/func(floor,func(pow,2,K-1))# : (b1'=#K#) & (r1'=r) & (b'=max(b,#K#));
+	[] go & draw & draw1=0 -> (draw1'=1);
+	[] draw1=1 ->#+ i=1:K-1# #1/func(floor,func(pow,2,i))# : (b1'=#i#) & (r1'=r) & (b'=max(b,#i#)) & (draw1'=0)
+	         #end#+ #1/func(floor,func(pow,2,K-1))# : (b1'=#K#) & (r1'=r) & (b'=max(b,#K#)) & (draw1'=0);
 	// enter critical section and randomly set r to 1 or 2
-	[] p1=1 & b=b1 & r=r1 & c=0 -> 0.5 : (r'=1) & (c'=1) & (b'=0) & (b1'=0) & (r1'=0) & (p1'=2)
+	[] go & p1=1 & b=b1 & r=r1 & c=0 -> 0.5 : (r'=1) & (c'=1) & (b'=0) & (b1'=0) & (r1'=0) & (p1'=2)
 	                             + 0.5 : (r'=2) & (c'=1) & (b'=0) & (b1'=0) & (r1'=0) & (p1'=2);
 	// loop when trying and cannot make a draw or enter critical section
-	[] p1=1 & r1=r & b1<=b & ((c=0 & b1!=b) | c=1) -> (p1'=p1);
+	// [] go & p1=1 & r1=r & b1<=b & ((c=0 & b1!=b) | c=1) -> (p1'=p1);
 	// leave crictical section
-	[] p1=2 -> (p1'=0) & (c'=0);
+	[] go & p1=2 -> (p1'=0) & (c'=0);
 	// stay in critical section
-	[] p1=2 -> (p1'=2);
+	// [] go & p1=2 -> (p1'=2);
 	
 endmodule
 
 // construct further modules through renaming
 #for i=2:N#
-module process#i# = process1 [p1=p#i#, b1=b#i#, r1=r#i#] endmodule#end#
+module process#i# = process1 [p1=p#i#, b1=b#i#, r1=r#i#, draw1=draw#i#, draw#i#=draw1 ] endmodule#end#
 
 // formulas/labels for use in properties:
 
@@ -69,3 +86,6 @@ label "one_trying" = #| i=1:N#p#i#=1#end#;
 
 // one of the processes is in the critical section
 label "one_critical" = #| i=1:N#p#i#=2#end#;
+
+// maximum value of the bi's
+formula maxb = max(b1#for i=2:N#,b#i##end#);
