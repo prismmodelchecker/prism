c38ee3656207752af9c8be266ee9e4ae6080c255
Dave Parker <dave.x.parker@gmail.com>
2010-01-21 22:42:45 +0000

    Some tidying of PTA examples.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@1695 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/examples/pta/csma-prism.sh
+++ prism/examples/pta/csma-prism.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 # abstract (time bounded)
 
--- prism/examples/pta/repudiation-prism.sh
+++ prism/examples/pta/repudiation-prism.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
 
 # honest
-prism-explicit repudiation/honest/{repudiation.nm,eventually.pctl} -aroptions refine=all,opt
-prism-explicit repudiation/honest/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=40
-prism-explicit repudiation/honest/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=80
-prism-explicit repudiation/honest/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=100
+prism repudiation/honest/{repudiation.nm,eventually.pctl} -aroptions refine=all,opt
+prism repudiation/honest/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=40
+prism repudiation/honest/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=80
+prism repudiation/honest/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=100
 
 # malicious
-prism-explicit repudiation/malicious/{repudiation.nm,eventually.pctl} -aroptions refine=all,opt
-prism-explicit repudiation/malicious/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=5
-prism-explicit repudiation/malicious/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=10
-prism-explicit repudiation/malicious/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=20
+prism repudiation/malicious/{repudiation.nm,eventually.pctl} -aroptions refine=all,opt
+prism repudiation/malicious/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=5
+prism repudiation/malicious/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=10
+prism repudiation/malicious/{repudiation.nm,deadline.pctl} -aroptions refine=all,opt -const T=20
 
