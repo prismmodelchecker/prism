745794c57ac5e167efe5ea81cef5da362a995789
Dave Parker <dave.x.parker@gmail.com>
2010-10-15 15:57:33 +0000

    Put PTA files in main examples dir.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@2144 bbc10eb1-c90d-0410-af57-cb519fbb1720

---
+++ prism-examples/pta/tests-prism.sh
@@ -0,0 +1,9 @@
+
+prism-benchmark -exec prism-explicit -logs logs csma
+prism-benchmark -exec prism-explicit -logs logs csma/abst
+prism-benchmark -exec prism-explicit -logs logs firewire/impl
+prism-benchmark -exec prism-explicit -logs logs firewire/abst
+prism-benchmark -exec prism-explicit -logs logs zeroconf
+prism-benchmark -exec prism-explicit -logs logs repudiation/honest
+prism-benchmark -exec prism-explicit -logs logs repudiation/malicious
+
