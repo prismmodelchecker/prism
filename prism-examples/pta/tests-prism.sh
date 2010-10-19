5c59047e2f4b33eb01fcae12fbdee483000b3bea
Dave Parker <dave.x.parker@gmail.com>
2010-10-19 08:57:04 +0000

    Tidy up of PTA examples.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@2169 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism-examples/pta/tests-prism.sh
+++
@@ -1,9 +0,0 @@
-
-prism-benchmark -exec prism-explicit -logs logs csma
-prism-benchmark -exec prism-explicit -logs logs csma/abst
-prism-benchmark -exec prism-explicit -logs logs firewire/impl
-prism-benchmark -exec prism-explicit -logs logs firewire/abst
-prism-benchmark -exec prism-explicit -logs logs zeroconf
-prism-benchmark -exec prism-explicit -logs logs repudiation/honest
-prism-benchmark -exec prism-explicit -logs logs repudiation/malicious
-
