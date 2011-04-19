adac14e1d82bd3ecfcb2f7e463a29b6ed33fd10b
Dave Parker <dave.x.parker@gmail.com>
2011-04-19 19:31:50 +0000

    Bugfix: GUI simulator now correctly recalls choices made in states.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@2813 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/simulator/TransitionList.java
+++ prism/src/simulator/TransitionList.java
@@ -34,7 +34,9 @@ import prism.*;
 public class TransitionList
 {
 	private ArrayList<Choice> choices = new ArrayList<Choice>();
+	/** The index of the choice containing each transition. */
 	private ArrayList<Integer> transitionIndices = new ArrayList<Integer>();
+	/** The offset with the choice containing each transition. */
 	private ArrayList<Integer> transitionOffsets = new ArrayList<Integer>();
 	private int numChoices = 0;
 	private int numTransitions = 0;
@@ -140,7 +142,7 @@ public class TransitionList
 	 */
 	public int getTotalIndexOfTransition(int i, int offset)
 	{
-		return transitionOffsets.get(i);
+		return transitionIndices.indexOf(i) + offset;
 	}
 
 	// Random selection of a choice 
