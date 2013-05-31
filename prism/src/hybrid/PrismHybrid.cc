d1046f553b37eff1e7057d7b9dfb648d25786421
Dave Parker <dave.x.parker@gmail.com>
2013-05-31 23:59:40 +0000

    Typos (fix from Gaston Ingaramo).
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@6818 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/hybrid/PrismHybrid.cc
+++ prism/src/hybrid/PrismHybrid.cc
@@ -198,7 +198,7 @@ void PH_SetErrorMessage(const char *str, ...)
 	va_list argptr;
 	
 	va_start(argptr, str);
-	vsnprintf(error_message, MAX_LOG_STRING_LEN, str, argptr);
+	vsnprintf(error_message, MAX_ERR_STRING_LEN, str, argptr);
 	va_end(argptr);
 }
 
