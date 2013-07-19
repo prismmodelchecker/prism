1de1b052c6058f03852b6f1dab2e2423d17c43dd
Dave Parker <dave.x.parker@gmail.com>
2013-07-19 20:34:54 +0000

    Bug fix: Transitions counted incorrectly in DTMCSimplle.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@7095 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/explicit/DTMCSimple.java
+++ prism/src/explicit/DTMCSimple.java
@@ -188,7 +188,7 @@ public class DTMCSimple extends DTMCExplicit implements ModelSimple
 	public void setProbability(int i, int j, double prob)
 	{
 		Distribution distr = trans.get(i);
-		if (distr.get(i) != 0.0)
+		if (distr.get(j) != 0.0)
 			numTransitions--;
 		if (prob != 0.0)
 			numTransitions++;
