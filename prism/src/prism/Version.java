8f7913074dcfedc399be4381b613d8f0eb1e1630
Dave Parker <dave.x.parker@gmail.com>
2014-05-12 00:02:34 +0000

    Fix version for 4.2
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@8212 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/prism/Version.java
+++ prism/src/prism/Version.java
@@ -3,7 +3,7 @@ package prism;
 public class Version
 {
 	/** PRISM version (e.g. "4.0.3"). Just a single string (will be extracted by scripts). */
-	public static String versionString = "4.1";
+	public static String versionString = "4.2";
 	/** Optional PRISM version suffix (e.g. "dev", "beta"). Just a single string (will be extracted by scripts). */
-	public static String versionSuffixString = "dev";
+	public static String versionSuffixString = "";
 }
