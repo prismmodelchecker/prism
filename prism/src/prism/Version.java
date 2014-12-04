43a58adacae7550383739ddd9b9dff56745acfac
Dave Parker <dave.x.parker@gmail.com>
2014-12-04 12:23:50 +0000

    Version/changelog - 4.2.1.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@9375 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/prism/Version.java
+++ prism/src/prism/Version.java
@@ -3,7 +3,7 @@ package prism;
 public class Version
 {
 	/** PRISM version (e.g. "4.0.3"). Just a single string (will be extracted by scripts). */
-	public static String versionString = "4.2";
+	public static String versionString = "4.2.1";
 	/** Optional PRISM version suffix (e.g. "dev", "beta"). Just a single string (will be extracted by scripts). */
-	public static String versionSuffixString = "beta1";
+	public static String versionSuffixString = "";
 }
