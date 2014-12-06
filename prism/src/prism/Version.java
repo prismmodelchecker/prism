e001c3ac1dfbd3103061112e600325ba27ea5247
Dave Parker <dave.x.parker@gmail.com>
2014-12-06 23:35:27 +0000

    Back to "dev" version (post release).
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@9384 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/prism/Version.java
+++ prism/src/prism/Version.java
@@ -5,5 +5,5 @@ public class Version
 	/** PRISM version (e.g. "4.0.3"). Just a single string (will be extracted by scripts). */
 	public static String versionString = "4.2.1";
 	/** Optional PRISM version suffix (e.g. "dev", "beta"). Just a single string (will be extracted by scripts). */
-	public static String versionSuffixString = "";
+	public static String versionSuffixString = "dev";
 }
