835e95a861eed087b59291b72a013d01ab188ec9
Dave Parker <dave.x.parker@gmail.com>
2015-06-19 16:49:01 +0000

    Don't print optimal strategy to screen when exporting it (explicit engine).
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@10042 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/explicit/MDPModelChecker.java
+++ prism/src/explicit/MDPModelChecker.java
@@ -345,12 +345,6 @@ public class MDPModelChecker extends ProbModelChecker
 		if (exportAdv) {
 			// Prune strategy
 			restrictStrategyToReachableStates(mdp, strat);
-			// Print strategy
-			mainLog.print("Strat:");
-			for (i = 0; i < n; i++) {
-				mainLog.print(" " + i + ":" + strat[i]);
-			}
-			mainLog.println();
 			// Export
 			PrismLog out = new PrismFileLog(exportAdvFilename);
 			new DTMCFromMDPMemorylessAdversary(mdp, strat).exportToPrismExplicitTra(out);
@@ -1251,12 +1245,6 @@ public class MDPModelChecker extends ProbModelChecker
 		if (exportAdv) {
 			// Prune strategy
 			restrictStrategyToReachableStates(mdp, strat);
-			// Print strategy
-			mainLog.print("Strat:");
-			for (i = 0; i < n; i++) {
-				mainLog.print(" " + i + ":" + strat[i]);
-			}
-			mainLog.println();
 			// Export
 			PrismLog out = new PrismFileLog(exportAdvFilename);
 			new DTMCFromMDPMemorylessAdversary(mdp, strat).exportToPrismExplicitTra(out);
