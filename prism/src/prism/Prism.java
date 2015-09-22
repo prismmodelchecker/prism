b789d5ed5684c4f2c77a35d01bf0d58eaaffabdc
Dave Parker <dave.x.parker@gmail.com>
2015-09-22 21:39:13 +0000

    Code tidy.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@10713 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/prism/Prism.java
+++ prism/src/prism/Prism.java
@@ -32,10 +32,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
