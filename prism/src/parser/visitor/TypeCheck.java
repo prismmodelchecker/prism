2d8194398bb07c9b0582f00fbf398738df91d2b8
Vojtech Forejt <forejtv@gmail.com>
2015-12-22 12:28:39 +0000

    Fixed a bug where the type of multi was not correctly determined if boolean arguments came before double arguments. Now we enforce doubles to be given first.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@11076 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/parser/visitor/TypeCheck.java
+++ prism/src/parser/visitor/TypeCheck.java
@@ -371,11 +371,18 @@ public class TypeCheck extends ASTTraverse
 			}
 			break;
 		case ExpressionFunc.MULTI:
-			// All operands must be booleans or doubles
+			// All operands must be booleans or doubles, and doubles must come first.
+			boolean seenBoolean = false;
 			for (i = 0; i < n; i++) {
 				if (!(types[i] instanceof TypeBool || types[i] instanceof TypeDouble)) {
 					throw new PrismLangException("Type error: non-Boolean/Double argument to  function \"" + e.getName()
 							+ "\"", e.getOperand(i));
+				}
+				if (seenBoolean && types[i] instanceof TypeDouble) {
+					throw new PrismLangException("Type error: in the function \"" + e.getName() + "\", any Double arguments must come before any Boolean arguments.");
+				}
+				if (types[i] instanceof TypeBool) {
+					seenBoolean = true;
 				}
 			}
 			break;
