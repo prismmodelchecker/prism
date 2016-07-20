0e57919d0e1c08f11c6d83bc8b1e215148edc240
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2016-07-20 10:18:58 +0000

    explicit engine: Properly signal "not supported" for MDP total reward computations
    Previously,
    prism prism-examples/dice/two_dice.nm -pf 'Rmax=?[C]' -ex
    would yield a NullPointerException.
    
    
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@11522 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/explicit/ProbModelChecker.java
+++ prism/src/explicit/ProbModelChecker.java
@@ -1024,7 +1024,6 @@ public class ProbModelChecker extends NonProbModelChecker
 			res = ((CTMCModelChecker) this).computeTotalRewards((CTMC) model, (MCRewards) modelRewards);
 			break;
 		case MDP:
-			break;
 		default:
 			throw new PrismNotSupportedException("Explicit engine does not yet handle the " + expr.getOperatorSymbol() + " reward operator for " + model.getModelType()
 					+ "s");
