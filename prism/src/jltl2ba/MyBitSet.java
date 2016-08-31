338ef64bb672ad351f56f9fd3995b68345654124
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2016-08-31 07:16:06 +0000

    Revert SVN 11756 "add jltl2ba.MyBitSet.clone()", breaks LTL->NBA generation
    Sorry about that.
    
    
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/trunk@11762 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/jltl2ba/MyBitSet.java
+++ prism/src/jltl2ba/MyBitSet.java
@@ -111,14 +111,6 @@ public class MyBitSet extends BitSet implements Comparable<BitSet>, Iterable<Int
 		return (tmp1.length() - tmp2.length());
 	}
 
-	@Override
-	public MyBitSet clone()
-	{
-		MyBitSet result = new MyBitSet(_size);
-		result.or(this);
-		return result;
-	}
-
 	public void increment() {
 		int i = this.nextClearBit(0);
 		this.clear(0,i);
