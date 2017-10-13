c8bf2b0fe8d19ba2a3c0ebfec0991478d04cd87d
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2017-10-13 15:41:18 +0200

    refactor: TypeDouble expressions may evaluate to BigRational, add proper casts
    This allows storage of BigRational values in StateValues / Values vectors, e.g.,
    to store constants that have been evaluated exactly.
    
    TypeDouble.castValueTo now returns a Number instead of a Double, requiring the use
    of the doubleValue() method in several places where the value is evaluated using
    double arithmetic.

--- prism/src/parser/type/TypeDouble.java
+++ prism/src/parser/type/TypeDouble.java
@@ -26,6 +26,7 @@
 
 package parser.type;
 
+import param.BigRational;
 import prism.PrismLangException;
 
 public class TypeDouble extends Type 
@@ -70,10 +71,12 @@ public class TypeDouble extends Type
 	}
 	
 	@Override
-	public Double castValueTo(Object value) throws PrismLangException
+	public Number castValueTo(Object value) throws PrismLangException
 	{
 		if (value instanceof Double)
 			return (Double) value;
+		if (value instanceof BigRational)
+			return (BigRational) value;
 		if (value instanceof Integer)
 			return new Double(((Integer) value).intValue());
 		else
