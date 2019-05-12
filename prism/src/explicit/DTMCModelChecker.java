prismmodelchecker/prism

commit 809f10fe9ba6f5f2638c74529fb51cf232586423

Provide method to compute steady state probs for BSCC without explicit post processing

prism/src/explicit/DTMCModelChecker.java
@@ -2422,6 +2422,14 @@ public class DTMCModelChecker extends ProbModelChecker
 	}
 
 	/**
+	 * @see DTMCModelChecker#computeSteadyStateProbsForBSCC(DTMC, BitSet, double[], BSCCPostProcessor)
+	 */
+	public ModelCheckerResult computeSteadyStateProbsForBSCC(DTMC dtmc, BitSet states, double result[]) throws PrismException
+	{
+		return computeSteadyStateProbsForBSCC(dtmc, states, result, null);
+	}
+
+	/**
 	 * Compute steady-state probabilities for a BSCC
 	 * i.e. compute the long-run probability of being in each state of the BSCC.
 	 * No initial distribution is specified since it does not affect the result.

prism/src/explicit/CTMCModelChecker.java
@@ -861,6 +861,18 @@ public class CTMCModelChecker extends ProbModelChecker
 	}
 
 	/**
+	 * @see DTMCModelChecker#computeSteadyStateProbsForBSCC(DTMC, BitSet, double[], BSCCPostProcessor)
+	 */
+	public ModelCheckerResult computeSteadyStateProbsForBSCC(CTMC ctmc, BitSet states, double result[]) throws PrismException
+	{
+		// We construct the embedded DTMC and do the steady-state computation there
+		mainLog.println("Building embedded DTMC...");
+		DTMC dtmcEmb = ctmc.getImplicitEmbeddedDTMC();
+
+		return createDTMCModelChecker().computeSteadyStateProbsForBSCC(dtmcEmb, states, result, new SteadyStateBSCCPostProcessor(ctmc));
+	}
+
+	/**
 	 * Compute steady-state rewards, i.e., R=?[ S ].
 	 * @param ctmc the CTMC
 	 * @param modelRewards the (state) rewards

commit 43b22d170bb9cae079a46bca9b7b69bf92a2c78c

CTMC steady explicit: Fix regression introduced by #116
PR #116 (in particular 5c33a555ac85465393c30609e0f253cfd1b839aa) moved
the post-processing of the BSCC steady state probabilities (weighing
by the exit rates of the CTMC) into computeSteadyStateProbsForBSCC().

This introduced a regression, leading to wrong results, as the
post-processing would be applied on the solution vector that was
returned by the method, but not necessarily on the values passed back
out via the result argument vector.

We fix this by moving the post-processing up, before the copy and
adding a note of caution.

Failure e.g. for
prism prism-tests/papers/Par02/Par02-3.3.3.sm prism-tests/papers/Par02/Par02-3.3.3.sm.props -explicit -testall

prism/src/explicit/DTMCModelChecker.java
@@ -2553,6 +2553,11 @@ public class DTMCModelChecker extends ProbModelChecker
 			iterationsExport.close();
 		}
 
+		// Apply post processing on soln
+		if (bsccPostProcessor != null) {
+			bsccPostProcessor.apply(soln, states);
+		}
+
 		if (result != null && result != soln) {
 			// If result vector was passed in as method argument,
 			// it can be the case that result does not point to the current soln vector (most recent values)
@@ -2563,6 +2568,10 @@ public class DTMCModelChecker extends ProbModelChecker
 				result[state] = soln[state];
 			}
 		}
+		//
+		// NOTE: from here on, don't change the values of result/soln,
+		// as the values may have already been copied to the result vector above
+		//
 		// store only one result vector, free temporary vectors
 		result = soln;
 		soln = soln2 = null;
@@ -2572,11 +2581,6 @@ public class DTMCModelChecker extends ProbModelChecker
 			String msg = "Iterative method did not converge within " + iters + " iterations.\n" +
 			             "Consider using a different numerical method or increasing the maximum number of iterations";
 			throw new PrismException(msg);
-		}
-
-		// Apply post processing
-		if (bsccPostProcessor != null) {
-			bsccPostProcessor.apply(result, states);
 		}
 
 		// Return results