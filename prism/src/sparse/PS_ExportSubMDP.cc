95302e9440e3680974c9d0b00551356383475e48
Steffen Märcker <steffen.maercker@tu-dresden.de>
2022-02-07 17:04:28 +0100

    Preserve double precision on model export (symbolic)
    Fixes #175 for the symbolic engines

--- prism/src/sparse/PS_ExportSubMDP.cc
+++ prism/src/sparse/PS_ExportSubMDP.cc
@@ -109,9 +109,9 @@ jstring fn		// filename
 			if (export_type == EXPORT_ROWS) export_string("%d", i);
 			for (k = l2; k < h2; k++) {
 				switch (export_type) {
-				case EXPORT_PLAIN: export_string("%d %d %d %.12g\n", i, j-l1, cols[k], non_zeros[k]); break;
-				case EXPORT_MATLAB: export_string("%s%d(%d,%d)=%.12g;\n", export_name, j-l1+1, i+1, cols[k]+1, non_zeros[k]); break;
-				case EXPORT_ROWS: export_string(" %.12g:%d", non_zeros[k], cols[k]); break;
+				case EXPORT_PLAIN: export_string("%d %d %d %.17g\n", i, j-l1, cols[k], non_zeros[k]); break;
+				case EXPORT_MATLAB: export_string("%s%d(%d,%d)=%.17g;\n", export_name, j-l1+1, i+1, cols[k]+1, non_zeros[k]); break;
+				case EXPORT_ROWS: export_string(" %.17g:%d", non_zeros[k], cols[k]); break;
 				}
 			}
 			if (export_type == EXPORT_ROWS) export_string("\n");
