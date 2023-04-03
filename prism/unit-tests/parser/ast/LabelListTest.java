1d56c7996b442886dfc98ea5394ecdf8f1a48a7b
Max Kurze <Max.Kurze@mailbox.tu-dresden.de>
2023-04-03 21:06:46 +0200

    prism-tests for labels inside labels and guards

---
+++ prism/unit-tests/parser/ast/LabelListTest.java
@@ -0,0 +1,109 @@
+package parser.ast;
+
+import org.junit.jupiter.api.Test;
+import parser.State;
+import parser.type.TypeInt;
+import prism.PrismLangException;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LabelListTest
+{
+    @Test
+    public void testFindCycles_emptyList()
+    {
+        LabelList ll = new LabelList();
+        assertDoesNotThrow(ll::findCycles);
+    }
+
+    @Test
+    public void testFindCycles_1LabelCycle()
+    {
+        LabelList ll = createLabelListWith1LabelCycle();
+        assertThrows(PrismLangException.class, ll::findCycles);
+    }
+
+    @Test
+    public void testFindCycles_2LabelCycle()
+    {
+        LabelList ll = createLabelListWith2LabelCycle();
+        assertThrows(PrismLangException.class, ll::findCycles);
+    }
+
+    @Test
+    public void testFindCycles_noCycle()
+    {
+        LabelList ll = createLabelListWithoutCycle();
+        assertDoesNotThrow(ll::findCycles);
+    }
+
+    @Test
+    public void testGetLabelValues() throws PrismLangException
+    {
+        /*
+        d is part of the state and will go from [0..5]
+
+        label "l1" = "l2" | d=5
+        label "l2" = d<=3
+         */
+        LabelList ll = createLabelListWithoutCycle();
+
+        // loop through all states and check label values
+        for (int i = 0; i <= 5; i++) {
+            // create the new state with 1 variable
+            State state = new State(1);
+            // set d to [0..5]
+            state.setValue(0, i); // index of d is 0
+
+            // expected labelValues
+            boolean l2 = i <= 3;
+            boolean l1 = l2 || i == 5;
+
+            // compare
+            Map<String, Boolean> actualValues = ll.getLabelValues(state);
+            assertEquals(l1, actualValues.get("l1"));
+            assertEquals(l2, actualValues.get("l2"));
+        }
+    }
+
+    private LabelList createLabelListWithoutCycle()
+    {
+        /*
+        label "l1" = "l2" | d=5
+        label "l2" = d<=3
+         */
+        LabelList ll = new LabelList();
+        ExpressionVar dVar = new ExpressionVar("d", TypeInt.getInstance());
+        dVar.setIndex(0); // index of d is 0
+        ll.addLabel(new ExpressionIdent("l1"), new ExpressionBinaryOp(ExpressionBinaryOp.OR,
+                new ExpressionLabel("l2"), new ExpressionBinaryOp(ExpressionBinaryOp.EQ,
+                dVar, new ExpressionLiteral(TypeInt.getInstance(), 5))));
+        ll.addLabel(new ExpressionIdent("l2"), new ExpressionBinaryOp(ExpressionBinaryOp.LE,
+                dVar, new ExpressionLiteral(TypeInt.getInstance(), 3)));
+        return ll;
+    }
+
+    private LabelList createLabelListWith1LabelCycle()
+    {
+        LabelList ll = new LabelList();
+        /* label "l1" = "l1" */
+        ll.addLabel(new ExpressionIdent("l1"), new ExpressionLabel("l1"));
+        return ll;
+    }
+
+    private LabelList createLabelListWith2LabelCycle()
+    {
+        LabelList ll = new LabelList();
+        /*
+        label "l1" = "l2"
+        label "l2" = "l1" | d=5
+         */
+        ll.addLabel(new ExpressionIdent("l1"), new ExpressionLabel("l2"));
+        ll.addLabel(new ExpressionIdent("l2"), new ExpressionBinaryOp(ExpressionBinaryOp.OR,
+                new ExpressionLabel("l1"), new ExpressionBinaryOp(ExpressionBinaryOp.EQ,
+                new ExpressionVar("d", TypeInt.getInstance()), new ExpressionLiteral(TypeInt.getInstance(), 5))));
+        return ll;
+    }
+}
