86088df0d7190f474fbff40c632b8782b4e243b7
dragos <marin.dragos296@gmail.com>
2023-06-23 00:05:27 +0300

    simple use of gurobi lp solver

--- prism/src/explicit/IPOMDPModelChecker.java
+++ prism/src/explicit/IPOMDPModelChecker.java
@@ -48,6 +48,7 @@ import strat.FMDStrategyProduct;
 import strat.MDStrategy;
 import strat.MDStrategyArray;
 import strat.Strategy;
+import gurobi.*;
 
 /**
  * Explicit-state model checker for interval Markov decision prcoesses (IMDPs).
@@ -97,6 +98,48 @@ public class IPOMDPModelChecker extends ProbModelChecker
 					mainLog.println(s + "," + i + " -> " + elem.getValue() + ":" + elem.getKey());
 				}
 			}
+		}
+
+		try {
+			GRBEnv env = new GRBEnv("IPOMDP.log");
+			GRBModel model = new GRBModel(env);
+
+			// Create variables
+			GRBVar x = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "x");
+			GRBVar y = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "y");
+			GRBVar z = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "z");
+
+			// Set objective: maximize x + y + 2 z
+			GRBLinExpr expr = new GRBLinExpr();
+			expr.addTerm(1.0, x); expr.addTerm(1.0, y); expr.addTerm(2.0, z);
+			model.setObjective(expr, GRB.MAXIMIZE);
+
+			// Add constraint: x + 2 y + 3 z <= 4
+			expr = new GRBLinExpr();
+			expr.addTerm(1.0, x); expr.addTerm(2.0, y); expr.addTerm(3.0, z);
+			model.addConstr(expr, GRB.LESS_EQUAL, 4.0, "c0");
+
+			// Add constraint: x + y >= 1
+			expr = new GRBLinExpr();
+			expr.addTerm(1.0, x); expr.addTerm(1.0, y);
+			model.addConstr(expr, GRB.GREATER_EQUAL, 1.0, "c1");
+
+			// Optimize model
+			model.optimize();
+
+			System.out.println(x.get(GRB.StringAttr.VarName)
+					+ " " +x.get(GRB.DoubleAttr.X));
+			System.out.println(y.get(GRB.StringAttr.VarName)
+					+ " " +y.get(GRB.DoubleAttr.X));
+			System.out.println(z.get(GRB.StringAttr.VarName)
+					+ " " +z.get(GRB.DoubleAttr.X));
+
+			System.out.println("Obj: " + model.get(GRB.DoubleAttr.ObjVal));
+
+			model.dispose();
+			env.dispose();
+		} catch (GRBException e) {
+			throw new PrismException("Error solving LP: " + e.getMessage());
 		}
 
 		// Return dummy result vector
