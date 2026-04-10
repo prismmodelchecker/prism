d12867cae046800c7336404e237ba16b021fbaec
Dave Parker <dave.x.parker@gmail.com>
2026-04-10 12:23:12 +0100

    Update printversion.sh to match Prism.getVersion(): use - not .

--- prism/src/scripts/printversion.sh
+++ prism/src/scripts/printversion.sh
@@ -17,7 +17,7 @@ if [ -f "$PRISM_SRC_DIR"/prism/Version.java ]; then
 
 		VERSION_SUFFIX=`grep versionSuffixString "$PRISM_SRC_DIR"/prism/Version.java | sed -E 's/[^"]+"([^"]*)"[^"]+/\1/'`
 		if [ "$VERSION_SUFFIX" != "" ]; then
-			/bin/echo -n ".$VERSION_SUFFIX"
+			/bin/echo -n "-$VERSION_SUFFIX"
 	
 			if [ -f "$PRISM_SRC_DIR"/prism/Revision.java ]; then
 				REVISION_SVN=`grep svnRevision "$PRISM_SRC_DIR"/prism/Revision.java | sed -E 's/[^"]+"([^"]*)"[^"]+/\1/'`
